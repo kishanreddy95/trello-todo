5b356da8074a400d8e92ce65f5ee34d10ab92efd
kishan reddy <kishanreddy95@gitlab.com>
2018-09-12 12:14:55 +0530

    Add eslint in air-bnb config

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
+
 const app = express();
 
 // app.get('/', (req, res) => res.send('hello world'));
 
-let obj = {
-    data: {
-        name: "kishan",
-        age: 23
-    }
-}
-app.post('/', function (req, res) {
-    
-})
+const obj = {
+  data: {
+    name: 'kishan',
+    age: 23,
+  },
+};
+app.post('/', (req, res) => {
+  res.send(obj);
+});
 
 app.listen(3000, () => console.log('listening on port 3000'));
