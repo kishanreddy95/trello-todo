bedd0ff147495f5af2d5a8a26bf4c8e04015b80c
kishan reddy <kishanreddy95@gitlab.com>
2018-09-13 12:03:04 +0530

    Latest changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // app.get('/', (req, res) => res.send('hello world'));
 
 const obj = {
-  data: {
+  data1: {
     name: 'kishan',
     age: 23,
   },
@@ -15,15 +15,27 @@ const obj = {
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/obj', (req, res) => {
-  console.log('here');
+// app.get('/obj', (req, res) => {
+//   console.log('here');
+//   res.send(obj);
+// });
+// app.post('/obj', (req, res) => {
+//   let abc =req.body;
+//   console.log(abc);
+//   let xyz =req.body;
+//   res.send(xyz);
+// });
+
+app.get('/lists', (req, res) => {
   res.send(obj);
 });
-app.post('/obj', (req, res) => {
-  let abc =req.body;
-  console.log(abc);
-  let xyz =req.body;
-  res.send(xyz);
+
+app.post('/lists', (req, res) => {
+  obj.data2 = req.body;
+  console.log(req.body);
+  res.send(obj.data2);
 });
+
+// app.get('/lists')
 
 app.listen(3000, () => console.log('listening on port 3000'));
